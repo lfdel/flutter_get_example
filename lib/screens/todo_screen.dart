4a8e567c332265d27d99b8217c078cf005f68e86
Leonardo <lfdel24@gmail.com>
2021-01-16 09:51:00 -0500

    maquetando la vista todo screeen

--- lib/screens/todo_screen.dart
+++ lib/screens/todo_screen.dart
@@ -3,8 +3,43 @@ import 'package:flutter/material.dart';
 class TodoScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Placeholder(),
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(8),
+        child: Column(
+          children: [
+            Expanded(
+                child: TextField(
+              autofocus: true,
+              decoration: InputDecoration(
+                hintText: "What do you want to accomplish? ",
+                border: InputBorder.none,
+                focusedBorder: InputBorder.none,
+              ),
+              style: TextStyle(fontSize: 25),
+              keyboardType: TextInputType.multiline,
+              maxLines: 999,
+            )),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                FlatButton(
+                  onPressed: () {},
+                  child: Text('CANCEL',
+                      style: TextStyle(color: Colors.white, fontSize: 16)),
+                  color: Colors.red,
+                ),
+                FlatButton(
+                  onPressed: () {},
+                  child: Text('ADD',
+                      style: TextStyle(color: Colors.white, fontSize: 16)),
+                  color: Colors.green,
+                )
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
