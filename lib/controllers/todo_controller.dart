56f1ed6c3efe6d9ff150eaa28a59d27550ea5adb
Leonardo <lfdel24@gmail.com>
2021-01-16 11:23:07 -0500

    guardando los todos con get store

--- lib/controllers/todo_controller.dart
+++ lib/controllers/todo_controller.dart
@@ -1,7 +1,22 @@
 import 'package:flutter_get_example/models/todo.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 class TodoController extends GetxController {
   var todos = <Todo>[].obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+
+    List storage = GetStorage().read("todos");
+    if (storage != null) {
+      todos = storage.map((json) => Todo.fromJson(json)).toList().obs;
+    }
+
+    ever(todos, (_) {
+      GetStorage().write("todos", todos.toList());
+    });
+  }
 }
