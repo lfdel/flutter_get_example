160504bdc045f82b769e2e1f2fe4b728476dd9fa
Leonardo <lfdel24@gmail.com>
2021-01-16 10:03:31 -0500

    agregando la lista de tareas en home_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_get_example/screens/home_screen.dart';
 import 'package:flutter_get_example/screens/todo_screen.dart';
 import 'package:get/get_navigation/get_navigation.dart';
 
 void main() {
   runApp(GetMaterialApp(
     debugShowCheckedModeBanner: false,
-    home: TodoScreen(),
+    //home: TodoScreen(),
+    home: HomeScreen(),
   ));
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_get_example/controllers/todo_controller.dart';
+import 'package:get/get.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    //
+    final todoController = Get.put(TodoController());
+    //
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Todo list"),
+      ),
+      body: Container(
+        child: Obx(() => ListView.separated(
+            itemBuilder: (context, index) => ListTile(),
+            separatorBuilder: (_, __) => Divider(),
+            itemCount: todoController.todos.length)),
+      ),
+    );
   }
 }
